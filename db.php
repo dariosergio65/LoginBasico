ca7a50a922346484f3ca509fc487454a3b84d324
dariosergio65 <dariosergio@hotmail.com>
2020-08-29 19:59:28 -0300

    prueba0 error: key does not contain a section: user urror: key does not contain a section: user

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+
+
+session_start();
+
+$db_host="localhost:3307";
+$db_nombre="servicios";
+$db_usuario="root";
+$db_contra="";
+
+$conn = mysqli_connect($db_host,$db_usuario,$db_contra,$db_nombre);
+
+?>
